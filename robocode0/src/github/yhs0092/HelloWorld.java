13b333f18905b7dbe6bc1496cea2a212589a5d59
yhs0092 <yhs0092@163.com>
2019-05-01 21:58:33 +0800

    add an auto tracing and firing tank

---
+++ robocode0/src/github/yhs0092/HelloWorld.java
@@ -0,0 +1,37 @@
+package github.yhs0092;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
+
+public class HelloWorld extends Robot {
+
+  private static final int MAX_GUN_ROTATION_DEGREE = 20;
+
+  double traceDegree = MAX_GUN_ROTATION_DEGREE;
+
+  @Override
+  public void run() {
+    setAdjustRadarForGunTurn(false);
+    setAdjustGunForRobotTurn(true);
+    while (true) {
+      if (traceDegree >= 0) {
+        turnGunRight(MAX_GUN_ROTATION_DEGREE);
+      } else {
+        turnGunLeft(MAX_GUN_ROTATION_DEGREE);
+      }
+    }
+  }
+
+  @Override
+  public void onScannedRobot(ScannedRobotEvent event) {
+    traceDegree = Utils.normalRelativeAngleDegrees(event.getBearing() - getGunHeading() + getHeading());
+    turnGunRight(traceDegree);
+    if (getGunHeat() <= 0) {
+      fire(3);
+    }
+    if (Utils.isNear(traceDegree, 0)) {
+      scan();
+    }
+  }
+}
